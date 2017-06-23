6eff0f54dfc98891a3e832ad5d82760af58be213
Kirk Byers <ktbyers@twb-tech.com>
2017-06-23 11:12:46 -0700

    Add presentation

--- jinja2_example/build_bgp.py
+++ jinja2_example/build_bgp.py
@@ -1,9 +1,16 @@
 from __future__ import print_function
 import jinja2
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from getpass import getpass
+
+USER = "pyclass"
+PASSWD = getpass()
 
 my_vars = {
     'juniper1':
             {
+                'management_ip': 'juniper1.twb-tech.com',
                 'ge_ip': '10.10.10.1',
                 'ge_netmask': '24',
                 'local_as': '101',
@@ -12,6 +19,7 @@ my_vars = {
             },
     'juniper2':
             {
+                'management_ip': 'juniper2.twb-tech.com',
                 'ge_ip': '10.10.10.2',
                 'ge_netmask': '24',
                 'local_as': '102',
@@ -25,11 +33,28 @@ template_file = 'build_bgp.j2'
 with open(template_file) as f:
     bgp_template = f.read()
 
-for router_id in my_vars:
+for router_id, router_vars in my_vars.items():
     template = jinja2.Template(bgp_template)
-    rtr_cfg = template.render(my_vars[router_id])
+    rtr_cfg = template.render(router_vars)
     print()
     print('-' * 60)
     print("This is router: {}".format(router_id))
     print(rtr_cfg)
     print()
+
+    # Connecting to device
+    host = router_vars['management_ip']
+    a_device = Device(host=host, user=USER, password=PASSWD)
+    a_device.open()
+    a_device.timeout = 90
+    cfg = Config(a_device)
+    cfg.load(rtr_cfg, format="text", merge=False)
+    diff = cfg.diff()
+    print(diff)
+    status = raw_input("Do you want to continue: ")
+    if diff:
+        print("Commit changes")
+        cfg.commit()
+    else:
+        print("No changes")
+
